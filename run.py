fbfa8e44193b58ec44ab7db5fabb374b98605ec9
alxfed <alxfed@gmail.com>
2021-10-23 11:02:38 -0500

    initial commit

--- run.py
+++ run.py
@@ -2,5 +2,8 @@
 """ SQLAlchemy 2.0
 """
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 
 engine = create_engine("sqlite:////media/alxfed/data/dbase/sqla2.sqlite", future=True)
+Session = sessionmaker(engine, future=True)
